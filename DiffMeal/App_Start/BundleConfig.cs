cc71c2a0a33a670323599b6b41bace0ec0f97319
wggCode <wgq@msn.cn>
2014-07-31 15:58:17 +0800

    2014-07-31

---
+++ DiffMeal/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace DiffMeal.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Css").Include(
+                "~/Content/Css/base.css"));
+            bundles.Add(new ScriptBundle("~/Js").Include(
+                "~/Content/Js/base.js"));
+        }
+    }
+}
